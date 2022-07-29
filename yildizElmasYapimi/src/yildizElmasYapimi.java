19e78740e47084c911efb8e3886e1a022902615b
İlayda Aydın <ilaydaayda2002@gmail.com>
2022-07-29 13:07:48 +0300

    döngülerJava101

--- yildizElmasYapimi/src/yildizElmasYapimi.java
+++ yildizElmasYapimi/src/yildizElmasYapimi.java
@@ -11,22 +11,20 @@ public class yildizElmasYapimi {
 
 
         for (int i = 1; i <= row; i++) {
-            for (int k = 1; k <= (row - i); k++) {
+            for (int k = 1; k <= (row - i); k++)
                 System.out.print(" ");
-            }
-            for (int j = 1; j <= (2 * i) - 1; j++) {
+            for (int j = 1; j <= (2 * i) - 1; j++)
                 System.out.print("*");
-            }
             System.out.println();
         }
 
-        for (int i = 1; i <= row; i++) {
-            for (int j = row; j > (row - i); j--) {
+        for(int i=row-1;i>=1;i--)
+        {
+            for(int j=1;j<=row-i;j++)
                 System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * (row - i) - 1); k++) {
+            for(int j=1;j<=2*i-1;j++)
                 System.out.print("*");
-            }
+
             System.out.println();
         }
     }
