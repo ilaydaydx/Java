2bc8bf02b797e68c841a0525d9d113cb1626700e
İlayda Aydın <ilaydaayda2002@gmail.com>
2022-08-01 12:54:14 +0300

    metotlarJava101

--- Metotlar-HesapMakinesi/src/hesapMakinesi_dowhile.java
+++ Metotlar-HesapMakinesi/src/hesapMakinesi_dowhile.java
@@ -5,24 +5,24 @@ public class hesapMakinesi_dowhile {
         Scanner scan = new Scanner(System.in);
         int number, result = 0, i = 1;
         while (true) {
-            System.out.print(i++ + ". sayı :");
+            System.out.print(i++ + ". number :");
             number = scan.nextInt();
             if (number == 0) {
                 break;
             }
             result += number;
         }
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nAddition:" + result + "\n----------");
     }
 
     static void minus() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Kaç adet sayı gireceksiniz :");
+        System.out.print("How many numbers will you enter? :");
         int counter = scan.nextInt();
         int number, result = 0;
 
         for (int i = 1; i <= counter; i++) {
-            System.out.print(i + ". sayı :");
+            System.out.print(i + ". number :");
             number = scan.nextInt();
             if (i == 1) {
                 result += number;
@@ -31,7 +31,7 @@ public class hesapMakinesi_dowhile {
             result -= number;
         }
 
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nSubtraction:" + result + "\n----------");
     }
 
     static void times() {
@@ -39,7 +39,7 @@ public class hesapMakinesi_dowhile {
         int number, result = 1, i = 1;
 
         while (true) {
-            System.out.print(i++ + ". sayı :");
+            System.out.print(i++ + ". number :");
             number = scan.nextInt();
 
             if (number == 1)
@@ -52,20 +52,20 @@ public class hesapMakinesi_dowhile {
             result *= number;
         }
 
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nMultiplication:" + result + "\n----------");
     }
 
     static void divided() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Kaç adet sayı gireceksiniz :");
+        System.out.print("How many numbers will you enter?");
         int counter = scan.nextInt();
         double number, result = 0.0;
 
         for (int i = 1; i <= counter; i++) {
-            System.out.print(i + ". sayı :");
+            System.out.print(i + ". number :");
             number = scan.nextDouble();
             if (i != 1 && number == 0) {
-                System.out.println("Böleni 0 giremezsiniz.");
+                System.out.println("This operation cannot be done. Please enter a number other than 0.");
                 continue;
             }
             if (i == 1) {
@@ -75,14 +75,14 @@ public class hesapMakinesi_dowhile {
             result /= number;
         }
 
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nDivision:" + result + "\n----------");
     }
 
     static void power() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Taban değeri giriniz :");
+        System.out.print("Enter the base value :");
         int base = scan.nextInt();
-        System.out.print("Üs değeri giriniz :");
+        System.out.print("Enter the exponent value :");
         int exponent = scan.nextInt();
         int result = 1;
 
@@ -90,12 +90,12 @@ public class hesapMakinesi_dowhile {
             result *= base;
         }
 
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nExponent Calculation:" + result + "\n----------");
     }
 
     static void factorial() {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Sayı giriniz :");
+        System.out.print("Enter the number :");
         int n = scan.nextInt();
         int result = 1;
 
@@ -103,26 +103,48 @@ public class hesapMakinesi_dowhile {
             result *= i;
         }
 
-        System.out.println("Sonuç : " + result);
+        System.out.println("----------\nFactorial:" + result + "\n----------");
+    }
+
+    static void mod(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int n=scan.nextInt();
+        System.out.println("Enter the second number:");
+        int m= scan.nextInt();
+
+        int result=n%m;
+        System.out.println("----------\nMod is:" + result + "\n----------");
+    }
+
+    static void rectangular(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the long edge:");
+        int n=scan.nextInt();
+        System.out.println("Enter the short edge:");
+        int m= scan.nextInt();
+
+        System.out.println("----------\nRectangular area:"+ (m * n));
+        System.out.println("Rectangular perimeter:"+(2 * (m + n))+"\n----------" );
     }
 
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int select;
-        String menu = "1- Toplama İşlemi\n"
-                + "2- Çıkarma İşlemi\n"
-                + "3- Çarpma İşlemi\n"
-                + "4- Bölme işlemi\n"
-                + "5- Üslü Sayı Hesaplama\n"
-                + "6- Faktoriyel Hesaplama\n"
-                + "7- Mod Alma\n"
-                + "8- Dikdörtgen Alan ve Çevre Hesabı\n"
-                + "0- Çıkış Yap";
+        String menu = "\n1- Addition\n"
+                + "2- Subtraction\n"
+                + "3- Multiplication\n"
+                + "4- Division\n"
+                + "5- Exponent Calculation\n"
+                + "6-Factorial\n"
+                + "7- Get Mode\n"
+                + "8- Rectangular Area and Perimeter Calculation\n"
+                + "0- Log out";
+        System.out.println(menu);
 
         do {
-            System.out.println(menu);
-            System.out.print("Lütfen bir işlem seçiniz :");
+            System.out.println("\nSelect the transaction:");
             select = scan.nextInt();
             switch (select) {
                 case 1:
@@ -143,10 +165,16 @@ public class hesapMakinesi_dowhile {
                 case 6:
                     factorial();
                     break;
+                case 7:
+                    mod();
+                    break;
+                case 8:
+                    rectangular();
+                    break;
                 case 0:
                     break;
                 default:
-                    System.out.println("Yanlış bir değer girdiniz, tekrar deneyiniz.");
+                    System.out.println("You entered an invalid transaction.");
             }
         } while (select != 0);
 
